677a41a0eb2cc2423832c520e73ec4846f9802cc
ZuLove <249056903@qq.com>
2014-05-05 11:05:32 +0800

    add some example in this file

--- string_capwords.py
+++ string_capwords.py
@@ -24,3 +24,11 @@ Escape          : %%
 Variable in text: %(var)siable
 """
 print 'interpolation:',s % values
+
+t= string.Template("$var is here but $missing is not provided")
+
+try:
+    print 'substitute() :',t.substitute(values)
+except KeyError,err:
+    print 'ERROR:',str(err)
+print 'safe_substitute():',t.safe_substitute(values)
