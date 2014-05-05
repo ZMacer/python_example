d41043cf9cf1116047116320d4cfca554c333aab
ZuLove <249056903@qq.com>
2014-05-05 11:41:38 +0800

    add string_template_advanced.py file

---
+++ string_template_advanced.py
@@ -0,0 +1,42 @@
+#string_template_advanced.py
+#! /usr/bin/python
+# -*- coding:utf8 -*-
+
+import string
+
+template_text = ''' 
+    Replaced  : %with_underscore
+    Delimiter : %%
+    Ignored   : %notunderscored
+ '''
+d = { 'with_underscore':'replaced',
+      'notunderscored':'not replaced',
+      }
+class MyTemplate(string.Template):
+    delimiter = '%'
+    idpattern = '[a-z]+_[a-z]+'
+
+t = MyTemplate(template_text)
+print 'Modified ID pattern:'
+print t.safe_substitute(d)
+
+t = string.Template('$var')
+print t.pattern.pattern
+
+import re
+class MyTemplate(string.Template):
+    delimiter = '{{'
+    pattern = r'''
+    \{\{(?:
+    (?P<escaped>\{\{) |
+    (?P<named>[_a-z][_a-z0-9]*)\}\}|
+    (?P<braced>[_a-z][_a-z0-9]*)\}\}|
+    (?P<invalid>)
+    )
+    '''
+t = MyTemplate('''
+{{{{
+{{var}}
+''')
+print 'matches:', t.pattern.findall(t.template)
+print 'substituted:',t.safe_substitute(var='replacement')
